1161e9b4742cfd4ab86287af97c8dec5d253ddd1
s-razoes <49306008+s-razoes@users.noreply.github.com>
2022-09-21 12:50:30 +0100

    Network alarm for new devices to the network to bot

---
+++ networkAlarm.sh
@@ -0,0 +1,25 @@
+#don't forget to change these
+UDP_SERVER=127.0.0.1
+UDP_PORT=2001
+network=192.168.1.
+
+array=( $( for i in $(seq 254); do ping -c1 -W1 $network$i & done | grep from|sed 's/64 bytes from //'|sed 's/:.*//' ) )
+file="known_mac_addresses.txt"
+result=($(arp|grep -vi incomplete|awk '{ print $3 }'|tail -n +2))
+
+for i in "${array[@]}"
+do
+        echo "$i"
+done
+
+for i in "${result[@]}"
+do
+  if grep $i $file; then
+    echo "Known $i"
+  else
+    rst=$(arp|awk '{ print $1, $3 }'|tail -n +2|grep $i)
+		url=$(echo "$i"|sed "s/:/%253A/g")
+    echo "New in network > $rst https://dnschecker.org/mac-lookup.php?query=$url"|nc -u $UDP_SERVER $UDP_PORT
+    echo "$i" >> $file					
+  fi
+done
